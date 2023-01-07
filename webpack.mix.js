798003a01b2d2368384e7241c42a8d8d9f2862f9
jjayaraaj <jjayaraaj@gmail.com>
2023-01-07 08:03:27 +0530

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,8 @@
+// webpack.mix.js
+
+let mix = require("laravel-mix");
+
+// mix.js("src/app.js", "public/js").setPublicPath("dist");
+mix
+  .js("src/app.js", "public/js")
+  .postCss("src/app.css", "public/css", [require("tailwindcss")]);
